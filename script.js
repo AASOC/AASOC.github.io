b5b6e4d1749959866b60540155415b6844fa9316
Ru-Tao <necoarc@tutanota.com>
2025-09-06 10:10:07 +0100

    added calendar currently hidden, updated homepage, added events json for september, needs work

---
+++ script.js
@@ -0,0 +1,42 @@
+let data = {};
+
+
+let monthData;
+
+async function initCalendar() {
+	const response = await fetch("https://www.aasoced.moe/events.json");
+	const data = await response.json();
+	monthData = data["sept"]
+	for (let day in monthData) {
+		if (monthData[day].length !== 0) {
+			document.getElementById(day).style.color = "#0c9dff";
+		}
+		else {
+			document.getElementById(day).style.color = "#898989";
+		}
+	}
+}
+
+window.onload = initCalendar;
+
+function myFunction(element) {
+	let textContent = element.textContent;
+	let events = monthData[textContent]
+
+
+	console.log(events.length)
+	if (events.length === 0) {
+	    document.getElementById("info").innerHTML = "No events today.";
+	    return;
+	}
+
+	let text = "<ul>";
+	for (let i = 0; i < events.length; i++) {
+	 	text+= "<li>" + events[i]+ "</li>"
+	}
+	text += "<ul>";
+
+	document.getElementById("info").innerHTML = text;
+	document.getElementById("selectedDate").innerHTML = textContent + "  september";
+	
+}
